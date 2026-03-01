6895e066ac70b946532a7ad0035c4923427616ba
Terraform Dev <dev@example.com>
2026-03-01 17:17:45 +0000

    test(resource_governor): expand acceptance tests to cover all attributes

--- sqlserver/resource_resource_governor_test.go
+++ sqlserver/resource_resource_governor_test.go
@@ -25,3 +25,30 @@ resource "sqlserver_resource_governor" "test" {
   enabled = true
 }
 `
+
+func TestAccResourceResourceGovernor_full(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceResourceGovernorConfig_full,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("sqlserver_resource_governor.test", "enabled", "false"),
+					resource.TestCheckResourceAttr("sqlserver_resource_governor.test", "classifier_function", "my_classifier"),
+				),
+			},
+		},
+	})
+}
+
+const testAccResourceResourceGovernorConfig_full = `
+resource "sqlserver_classifier_function" "cf" {
+  name       = "my_classifier"
+  definition = "RETURN 'default';"
+}
+
+resource "sqlserver_resource_governor" "test" {
+  enabled = false
+  classifier_function = sqlserver_classifier_function.cf.name
+}
